39a4ffcc2f7ac9e5b7420a9ec5fce4d4c94faf42
DLarev06 <81909838+DLarev06@users.noreply.github.com>
2024-11-23 19:19:20 +0300

    Функциональное и классовое представление.

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import ClassView, func_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('classview/', ClassView.as_view()),
+    path('funcview/', func_view),
 ]
